5804acae6857c8f6f909226a232530bfd7bc8ab5
Evan Reid <evan_1995@hotmail.com>
2021-02-09 18:34:09 +0000

    finished but having trouble with insomnia tests

--- src/main/java/com/example/homework/oneToMany/controllers/UserController.java
+++ src/main/java/com/example/homework/oneToMany/controllers/UserController.java
@@ -1,4 +1,36 @@
 package com.example.homework.oneToMany.controllers;
 
+
+import com.example.homework.oneToMany.models.User;
+import com.example.homework.oneToMany.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping(value = "/users")
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> allUsers = userRepository.findAll();
+        return new ResponseEntity<>(allUsers, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/users/{id}")
+    public ResponseEntity<Optional<User>> getUser(@PathVariable Long id){
+        return new ResponseEntity<>(userRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createFolder (@RequestBody User user) {
+        userRepository.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
 }
--- src/test/java/com/example/homework/oneToMany/OneToManyApplicationTests.java
+++ src/test/java/com/example/homework/oneToMany/OneToManyApplicationTests.java
@@ -1,13 +1,43 @@
 package com.example.homework.oneToMany;
 
+import com.example.homework.oneToMany.models.File;
+import com.example.homework.oneToMany.models.Folder;
+import com.example.homework.oneToMany.models.User;
+import com.example.homework.oneToMany.repositories.FileRepository;
+import com.example.homework.oneToMany.repositories.FolderRepository;
+import com.example.homework.oneToMany.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OneToManyApplicationTests {
 
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	FolderRepository folderRepository;
+
+	@Autowired
+	FileRepository fileRepository;
+
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void canCreateAndSaveUsersFilesFolders(){
+
+
+	User john = new User("John");
+	userRepository.save(john);
+
+	Folder homeworks = new Folder("homework_folder", john);
+	folderRepository.save(homeworks);
+
+	File oneToMany = new File("oneToMany", ".java", 3, homeworks);
+	fileRepository.save(oneToMany);
+
+	}
 }
